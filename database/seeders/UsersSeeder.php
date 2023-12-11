d3b45c704d438421a26de481f99b9109fd06abd5
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2023-12-11 10:59:29 +0300

    örnek kullanıcı verilerinin listelendiği seeder

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'recep',
+            'surname' => 'gurbuz',
+            'tel' => '05132890123',
+            'address' => 'Istanbul Pendik',
+            'email' => 'recep@mail.com'
+        ]);
+
+        User::create([
+            'name' => 'bekir',
+            'surname' => 'gurbuz',
+            'tel' => '05132312299',
+            'address' => 'Istanbul Maltepe',
+            'email' => 'bekir@mail.com'
+        ]);
+    }
+}
