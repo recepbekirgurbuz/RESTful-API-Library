32550c2b4416350414d70ca8b3480ad6d43a66b6
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2023-12-16 17:52:13 +0300

    kullanicinin verdigi puanlarida ekranda gosterdik

--- app/Http/Controllers/Api/UserDetailsController.php
+++ app/Http/Controllers/Api/UserDetailsController.php
@@ -10,30 +10,48 @@ use App\Models\UserBook;
 
 class UserDetailsController extends Controller
 {
-    public function userDetails($id) {
-        $user = User::select('name', 'surname', 'tel')->find($id);
-
-        # $delivery den book_id sütununu aldım ve kitap id'sinden kitap adını çektim
-        $deliveryTrue = UserBook::select('book_id', 'user_id', 'status')->where('user_id', $id)->where('status', 'true')->get();
-
-        $deliveryBook = Book::select('book_name', 'author')->whereIn('id', $deliveryTrue->pluck('book_id'))->get();
-        $point = UserBook::select('point')->whereIn('id', $deliveryTrue->pluck('book_id'))->get();
-
-        $deliveryFalse = UserBook::select('book_id', 'delivery_date')->where('user_id', $id)->where('status', "false")->get();
-        $book = Book::select('book_name', 'author')->whereIn('id', $deliveryFalse->pluck('book_id'))->get();
-
-        if($user) {
-            return response()->json([
-                'success' => true,
-                'Kullanıcı Bilgileri'=> $user,
-                'Kullanıcı bu kitabı aldı ve hala teslim etmedi' => [
-                    'kitap adı' => $book->pluck('book_name'),
-                    'son teslim tarihi' => $deliveryFalse->pluck('delivery_date'),
-                ],
-                'Daha önce okuduğu kitaplar' => [
-                    $deliveryBook->pluck('book_name', 'author'),
-                ],
-            ]);
+    public function userDetails($userId) {
+        // Kullanıcı bilgilerini çek
+        $user = User::find($userId);
+        if (!$user) {
+            return response()->json(['error' => 'Böyle bir kullanıcı yok'], 404);
         }
+
+        $userBooks = UserBook::select('user_books.*', 'books.*')
+            ->join('books', 'user_books.book_id', 'books.id')
+            ->where('user_books.user_id', $userId)
+            ->where('user_id', $userId)
+            ->where('user_books.status', 'true')
+            ->get();
+        $notDelivered = UserBook::select('user_books.*', 'books.*')
+            ->join('books', 'user_books.book_id', '=', 'books.id')
+            ->where('user_books.user_id', $userId)
+            ->where('user_id', $userId)
+            ->where('user_books.status', 'false')
+            ->get();
+
+        $userData = [
+            'user_info' => [
+                'user_name' => $user->name,
+                'surname' => $user->surname,
+                'address' => $user->address,
+                'telefon' => $user->tel,
+            ],
+            'telim_edilmedi' => $notDelivered->map(function ($userBook) {
+                return [
+                    'book_name' => $userBook->book_name,
+                    'kullanicinin_verdigi_puan' => $userBook->point,
+                ];
+            }),
+            'teslim_edilen_onceki_kitaplar' => $userBooks->map(function ($userBook) {
+                return [
+                    'book_name' => $userBook->book_name,
+                    'kullanicinin_verdigi_puan' => $userBook->point,
+                ];
+            }),
+        ];
+
+        return response()->json($userData);
     }
 }
+
