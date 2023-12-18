411da0c6c23a38e0c07e78cdbe4aca45e476e009
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2023-12-18 14:39:20 +0300

    kitabın son teslim tarihi ekrana yazdırdık ve isimlendirmeler düzeltildi

--- app/Http/Controllers/Api/UserDetailsController.php
+++ app/Http/Controllers/Api/UserDetailsController.php
@@ -6,28 +6,27 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Book;
-use App\Models\UserBook;
+use App\Models\Delivery;
 
 class UserDetailsController extends Controller
 {
     public function userDetails($userId) {
-        // Kullanıcı bilgilerini çek
         $user = User::find($userId);
         if (!$user) {
             return response()->json(['error' => 'Böyle bir kullanıcı yok'], 404);
         }
 
-        $userBooks = UserBook::select('user_books.*', 'books.*')
-            ->join('books', 'user_books.book_id', 'books.id')
-            ->where('user_books.user_id', $userId)
+        $userBooks = Delivery::select('delivery.*', 'books.*')
+            ->join('books', 'delivery.book_id', 'books.id')
+            ->where('delivery.user_id', $userId)
             ->where('user_id', $userId)
-            ->where('user_books.status', 'true')
+            ->where('delivery.status', 'true')
             ->get();
-        $notDelivered = UserBook::select('user_books.*', 'books.*')
-            ->join('books', 'user_books.book_id', '=', 'books.id')
-            ->where('user_books.user_id', $userId)
+        $notDelivered = Delivery::select('delivery.*', 'books.*')
+            ->join('books', 'delivery.book_id', '=', 'books.id')
+            ->where('delivery.user_id', $userId)
             ->where('user_id', $userId)
-            ->where('user_books.status', 'false')
+            ->where('delivery.status', 'false')
             ->get();
 
         $userData = [
@@ -37,10 +36,11 @@ class UserDetailsController extends Controller
                 'address' => $user->address,
                 'telefon' => $user->tel,
             ],
-            'telim_edilmedi' => $notDelivered->map(function ($userBook) {
+            'teslim_edilmedi' => $notDelivered->map(function ($userBook) {
                 return [
                     'book_name' => $userBook->book_name,
                     'kullanicinin_verdigi_puan' => $userBook->point,
+                    'delivery_date' => $userBook->delivery_date,
                 ];
             }),
             'teslim_edilen_onceki_kitaplar' => $userBooks->map(function ($userBook) {
