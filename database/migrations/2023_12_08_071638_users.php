59b09d596a611f2cb8b7fe99a5691657b8bd7b93
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2023-12-26 00:00:08 +0300

    authentication ayarları yapıldı

--- database/migrations/2023_12_08_071638_users.php
+++ database/migrations/2023_12_08_071638_users.php
@@ -17,7 +17,9 @@ return new class extends Migration
             $table->string('surname');
             $table->string('address');
             $table->string('tel');
-            $table->string('email');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
