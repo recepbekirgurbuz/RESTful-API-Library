4701f2489a91505465c00ab208ac8024b1ad15e3
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2023-12-25 11:03:22 +0300

    tek bir kitap çekebilmesi için ilişkilendirme yapıldı

--- app/Models/Delivery.php
+++ app/Models/Delivery.php
@@ -31,4 +31,8 @@ class Delivery extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+    public function book()
+    {
+        return $this->belongsTo(Book::class, 'book_id');
+    }
 }
