32d737742c9639a4b069a89856c1121cd6281193
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2024-01-08 14:21:14 +0300

    router ayarlaması yapıldı

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,38 @@
-import VueRouter from "vue-router";
-import routes from "./routes";
+// router/index.js
 
-// configure router
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import DashboardLayout from "@/layout/dashboard/DashboardLayout.vue";
+import NotFound from "@/pages/NotFoundPage.vue";
+
+const Dashboard = () => import("@/pages/Dashboard.vue");
+const Profile = () => import("@/pages/Profile.vue");
+const Notifications = () => import("@/pages/Notifications.vue");
+const Icons = () => import("@/pages/Icons.vue");
+const Typography = () => import("@/pages/Typography.vue");
+const TableList = () => import("@/pages/TableList.vue");
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: DashboardLayout,
+    redirect: "/dashboard",
+    children: [
+      { path: "dashboard", name: "dashboard", component: Dashboard },
+      { path: "profile", name: "profile", component: Profile },
+      { path: "notifications", name: "notifications", component: Notifications },
+      { path: "icons", name: "icons", component: Icons },
+      { path: "table-list", name: "table-list", component: TableList }
+    ]
+  },
+  { path: "*", component: NotFound },
+];
+
 const router = new VueRouter({
-  routes, // short for routes: routes
-  linkExactActiveClass: "active",
-  scrollBehavior: (to) => {
-    if (to.hash) {
-      return {selector: to.hash}
-    } else {
-      return { x: 0, y: 0 }
-    }
-  }
+  routes,
+  mode: 'history', // Bu satır eklenmiştir
 });
 
 export default router;
